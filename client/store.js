391428a387ab458ad8c730083bca91537eada9dc
Derek Adams <d.3.1415926@gmail.com>
2018-08-20 17:25:03 -0700

    fix redux

--- client/store.js
+++ client/store.js
@@ -6,12 +6,10 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+const store = createStore(rootReducer,initialState,
+  applyMiddleware(...middleware),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //   )
   );
 
 export default store;
