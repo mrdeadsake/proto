ae02e432f38317ecc2ace8b6c76ee6181ea0e6c0
Derek Adams <d.3.1415926@gmail.com>
2018-08-23 21:01:02 -0700

    Order return

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    topics=Topic.all.map{|topic| {topic: topic, subtopics: topic.subtopics}}
-
-
+    topics=Topic.all.order("id ASC").map{|topic| {topic: topic, subtopics: topic.subtopics}}
     render({:json=>{topics: topics}})
   end
 
